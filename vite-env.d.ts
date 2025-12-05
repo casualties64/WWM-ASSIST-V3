903b374e36bd35dcf4a5f757f5f4ad0934e27502
casualties64 <casualties64@gmail.com>
2025-12-04 23:41:53 -0600

    test

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,4 +1,18 @@
-/// <reference types="vite/client" />
+// Removed broken reference to vite/client which was causing "Cannot find type definition file"
+// /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string;
+  readonly MODE: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  readonly SSR: boolean;
+  [key: string]: any;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 
 declare module '*.png' {
   const value: string;
